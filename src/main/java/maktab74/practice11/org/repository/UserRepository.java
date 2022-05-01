dbd73364bfd319ea145972ebdbac31de90ce0c6c
Hossein113 <hos_m18@yahoo.com>
2022-05-01 04:59:53 +0430

    update UserRepository

--- src/main/java/maktab74/practice11/org/repository/UserRepository.java
+++ src/main/java/maktab74/practice11/org/repository/UserRepository.java
@@ -2,6 +2,11 @@ package maktab74.practice11.org.repository;
 
 import maktab74.practice11.org.base.repository.BaseRepository;
 import maktab74.practice11.org.domain.User;
+import maktab74.practice11.org.utill.UserBrif;
 
 public interface UserRepository extends BaseRepository<User, Long> {
+
+    User loginByUserNameAndPassword(UserBrif userBrif);
+
+    void deletUser(User user);
 }
