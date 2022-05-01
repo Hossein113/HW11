24725c6a7d9c2164b72fa963bac14afa8a079c4d
Hossein113 <hos_m18@yahoo.com>
2022-05-02 00:28:20 +0430

    add LikeRepository

---
+++ src/main/java/maktab74/practice11/org/repository/LikeTwittRepository.java
@@ -0,0 +1,7 @@
+package maktab74.practice11.org.repository;
+
+import maktab74.practice11.org.base.repository.BaseRepository;
+import maktab74.practice11.org.domain.LikeTwitt;
+
+public interface LikeTwittRepository extends BaseRepository<LikeTwitt, Long> {
+}
