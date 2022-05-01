8ab68c18f2458aebcfc56827ddbfb735263bd4db
Hossein113 <hos_m18@yahoo.com>
2022-05-02 00:27:24 +0430

    add UserService

---
+++ src/main/java/maktab74/practice11/org/service/LikeTwittService.java
@@ -0,0 +1,7 @@
+package maktab74.practice11.org.service;
+
+import maktab74.practice11.org.base.service.BaseService;
+import maktab74.practice11.org.domain.LikeTwitt;
+
+public interface LikeTwittService extends BaseService<LikeTwitt, Long> {
+}
