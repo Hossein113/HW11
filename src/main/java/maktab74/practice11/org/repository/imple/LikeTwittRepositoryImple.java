2eaff8eff1b7d9a1eff53159e32c5bf9d695db8f
Hossein113 <hos_m18@yahoo.com>
2022-05-01 16:52:42 +0430

    update LikeTwittRepositoryImple

--- src/main/java/maktab74/practice11/org/repository/imple/LikeTwittRepositoryImple.java
+++ src/main/java/maktab74/practice11/org/repository/imple/LikeTwittRepositoryImple.java
@@ -2,10 +2,12 @@ package maktab74.practice11.org.repository.imple;
 
 import maktab74.practice11.org.base.repository.imple.BaseRepositoryImple;
 import maktab74.practice11.org.domain.LikeTwitt;
+import maktab74.practice11.org.repository.LikeTwittRepository;
 
 import javax.persistence.EntityManager;
 
-public class LikeTwittRepositoryImple extends BaseRepositoryImple<LikeTwitt, Long> {
+public class LikeTwittRepositoryImple extends BaseRepositoryImple<LikeTwitt, Long> implements
+        LikeTwittRepository {
     public LikeTwittRepositoryImple(EntityManager entityManager) {
         super(entityManager);
     }
@@ -13,5 +15,19 @@ public class LikeTwittRepositoryImple extends BaseRepositoryImple<LikeTwitt, Lon
     @Override
     public Class<LikeTwitt> entityClass() {
         return LikeTwitt.class;
+    }
+
+    @Override
+    public void likeOrDislike(LikeTwitt likeTwitt) {
+        entityManager.createQuery("update LikeTwitt l set l.likeTwitt=:myLike where l.id =: id ")
+                .setParameter("myLike", likeTwitt.getLikeTwitt()).setParameter("id", likeTwitt.getUserLike()).executeUpdate();
+    }
+
+    @Override
+    public LikeTwitt findByUserTwittLike(LikeTwitt likeTwitt) {
+        return entityManager.createQuery("select l from LikeTwitt l where l.userLike =: userLike" +
+                        " and l.twitt =: twitt", LikeTwitt.class)
+                .setParameter("userLike", likeTwitt.getUserLike())
+                .setParameter("twitt", likeTwitt.getTwitt()).getSingleResult();
     }
 }
