895392fc612f43067efc8d9d50a570c46a59eb0f
Hossein113 <hos_m18@yahoo.com>
2022-05-02 00:30:40 +0430

    update App

--- src/main/java/maktab74/practice11/org/Application.java
+++ src/main/java/maktab74/practice11/org/Application.java
@@ -1,4 +1,13 @@
 package maktab74.practice11.org;
 
+import maktab74.practice11.org.utill.HibernateUtill;
+
 public class Application {
+    public static void main(String[] args) {
+
+
+        System.out.println("start");
+        HibernateUtill.getEntityManagerFactory().createEntityManager();
+        System.out.println("END");
+    }
 }
