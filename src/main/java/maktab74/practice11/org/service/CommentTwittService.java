a6b31577122f090b9819b2add3275cf2988efa84
Hossein113 <hos_m18@yahoo.com>
2022-05-02 00:29:29 +0430

    added commentService

---
+++ src/main/java/maktab74/practice11/org/service/CommentTwittService.java
@@ -0,0 +1,8 @@
+package maktab74.practice11.org.service;
+
+import maktab74.practice11.org.base.service.BaseService;
+import maktab74.practice11.org.domain.CommentTwitt;
+
+public interface CommentTwittService extends BaseService<CommentTwitt, Long> {
+
+}
