c2120ae59b09834ec65e6c1b429b6c0574dfefe4
Hossein113 <hos_m18@yahoo.com>
2022-05-01 05:00:32 +0430

    update UserService

--- src/main/java/maktab74/practice11/org/service/UserService.java
+++ src/main/java/maktab74/practice11/org/service/UserService.java
@@ -2,6 +2,10 @@ package maktab74.practice11.org.service;
 
 import maktab74.practice11.org.base.service.BaseService;
 import maktab74.practice11.org.domain.User;
+import maktab74.practice11.org.utill.UserBrif;
 
 public interface UserService extends BaseService<User, Long> {
+    User loginByUserNameAndPassword(UserBrif userBrif);
+
+    void deletUser(User user);
 }
