f9d9fae5e2502cd34f79a4614977e65ef7aec524
Hossein113 <hos_m18@yahoo.com>
2022-05-02 00:25:37 +0430

    add User

--- src/main/java/maktab74/practice11/org/domain/User.java
+++ src/main/java/maktab74/practice11/org/domain/User.java
@@ -2,26 +2,40 @@ package maktab74.practice11.org.domain;
 
 import maktab74.practice11.org.base.domain.BaseEntity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user_tbl")
 public class User extends BaseEntity<Long> {
 
-    private String firstName;
-    private String lasttName;
-    private String usertName;
-    private Twitt twitt;
 
-    public User(Long aLong, String firstName, String lasttName, String usertName, Twitt twitt) {
-        super(aLong);
-        this.firstName = firstName;
-        this.lasttName = lasttName;
-        this.usertName = usertName;
-        this.twitt = twitt;
+    private String firstName;
+
+    private String lastName;
+
+    private String userName;
+
+    private String password;
+
+
+    public User() {
     }
 
-    public User(String firstName, String lasttName, String usertName, Twitt twitt) {
+    public User(Long aLong, String firstName, String lastName, String userName, String password) {
+        super(aLong);
         this.firstName = firstName;
-        this.lasttName = lasttName;
-        this.usertName = usertName;
-        this.twitt = twitt;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
+
+    }
+
+    public User(String firstName, String lastName, String userName, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
     }
 
     public String getFirstName() {
@@ -32,37 +46,38 @@ public class User extends BaseEntity<Long> {
         this.firstName = firstName;
     }
 
-    public String getLasttName() {
-        return lasttName;
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setLasttName(String lasttName) {
-        this.lasttName = lasttName;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getUsertName() {
-        return usertName;
+        return userName;
     }
 
     public void setUsertName(String usertName) {
-        this.usertName = usertName;
+        this.userName = usertName;
     }
 
-    public Twitt getTwitt() {
-        return twitt;
+    public String getPassword() {
+        return password;
     }
 
-    public void setTwitt(Twitt twitt) {
-        this.twitt = twitt;
+    public void setPassword(String password) {
+        this.password = password;
     }
+
 
     @Override
     public String toString() {
         return "User{" +
                 "firstName='" + firstName + '\'' +
-                ", lasttName='" + lasttName + '\'' +
-                ", usertName='" + usertName + '\'' +
-                ", twitt=" + twitt +
+                ", lastName='" + lastName + '\'' +
+                ", usertName='" + userName + '\'' +
+                ", password='" + password + '\'' +
                 "} " + super.toString();
     }
 }
