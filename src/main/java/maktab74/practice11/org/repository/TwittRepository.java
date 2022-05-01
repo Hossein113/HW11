ce2328a7c2339bbe465c12a0019e1c13dc06d9f1
Hossein113 <hos_m18@yahoo.com>
2022-05-01 16:54:11 +0430

    update TwittRepository

--- src/main/java/maktab74/practice11/org/repository/TwittRepository.java
+++ src/main/java/maktab74/practice11/org/repository/TwittRepository.java
@@ -3,5 +3,11 @@ package maktab74.practice11.org.repository;
 import maktab74.practice11.org.base.repository.BaseRepository;
 import maktab74.practice11.org.domain.Twitt;
 
+import java.util.List;
+
 public interface TwittRepository extends BaseRepository<Twitt, Long> {
+
+    List<Twitt> findByUser(Long id);
+
+    List<Twitt> findAllTwitt();
 }
