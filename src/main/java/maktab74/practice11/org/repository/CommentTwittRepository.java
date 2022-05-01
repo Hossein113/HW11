8f4a33b5a69103b0eb9721d36cce4d06b20dddd0
Hossein113 <hos_m18@yahoo.com>
2022-05-02 00:29:54 +0430

    added commentRepository

---
+++ src/main/java/maktab74/practice11/org/repository/CommentTwittRepository.java
@@ -0,0 +1,8 @@
+package maktab74.practice11.org.repository;
+
+import maktab74.practice11.org.base.repository.BaseRepository;
+import maktab74.practice11.org.domain.CommentTwitt;
+
+public interface CommentTwittRepository extends BaseRepository<CommentTwitt, Long> {
+
+}
