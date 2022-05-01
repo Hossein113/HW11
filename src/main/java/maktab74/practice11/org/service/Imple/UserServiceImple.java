225be63bef6b0706944a623f87444d39de602ec2
Hossein113 <hos_m18@yahoo.com>
2022-05-01 05:00:21 +0430

    update UserServiceImple

--- src/main/java/maktab74/practice11/org/service/Imple/UserServiceImple.java
+++ src/main/java/maktab74/practice11/org/service/Imple/UserServiceImple.java
@@ -4,10 +4,21 @@ import maktab74.practice11.org.base.service.imple.BaseServiceImple;
 import maktab74.practice11.org.domain.User;
 import maktab74.practice11.org.repository.UserRepository;
 import maktab74.practice11.org.service.UserService;
+import maktab74.practice11.org.utill.UserBrif;
 
 public class UserServiceImple extends BaseServiceImple<User, Long, UserRepository> implements UserService {
 
     public UserServiceImple(UserRepository repository) {
         super(repository);
+    }
+
+    @Override
+    public User loginByUserNameAndPassword(UserBrif userBrif) {
+        return repository.loginByUserNameAndPassword(userBrif);
+    }
+
+    @Override
+    public void deletUser(User user) {
+        repository.deletUser(user);
     }
 }
