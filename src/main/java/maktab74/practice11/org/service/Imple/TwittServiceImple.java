b50185d84c567f676d3b1b64b65d28471c7045cc
Hossein113 <hos_m18@yahoo.com>
2022-05-01 16:54:51 +0430

    update TwittServiceImple

--- src/main/java/maktab74/practice11/org/service/Imple/TwittServiceImple.java
+++ src/main/java/maktab74/practice11/org/service/Imple/TwittServiceImple.java
@@ -5,9 +5,21 @@ import maktab74.practice11.org.domain.Twitt;
 import maktab74.practice11.org.repository.TwittRepository;
 import maktab74.practice11.org.service.TwittService;
 
+import java.util.List;
+
 public class TwittServiceImple extends BaseServiceImple<Twitt, Long, TwittRepository>
         implements TwittService {
     public TwittServiceImple(TwittRepository repository) {
         super(repository);
+    }
+
+    @Override
+    public List<Twitt> findByUser(Long id) {
+        return repository.findByUser(id);
+    }
+
+    @Override
+    public List<Twitt> findAllTwitt() {
+        return repository.findAllTwitt();
     }
 }
