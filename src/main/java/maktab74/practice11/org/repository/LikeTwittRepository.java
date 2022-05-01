07021ea109127432a48ef0233cf1c209fccbba4d
Hossein113 <hos_m18@yahoo.com>
2022-05-01 16:52:18 +0430

    update LikeTwittRepository

--- src/main/java/maktab74/practice11/org/repository/LikeTwittRepository.java
+++ src/main/java/maktab74/practice11/org/repository/LikeTwittRepository.java
@@ -4,4 +4,8 @@ import maktab74.practice11.org.base.repository.BaseRepository;
 import maktab74.practice11.org.domain.LikeTwitt;
 
 public interface LikeTwittRepository extends BaseRepository<LikeTwitt, Long> {
+
+    void likeOrDislike(LikeTwitt likeTwitt);
+
+    LikeTwitt findByUserTwittLike(LikeTwitt likeTwitt);
 }
