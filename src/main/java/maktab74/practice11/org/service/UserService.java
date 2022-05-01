55da40c3f5d1a7050438b12e92fa4dfdeef07319
Hossein113 <hos_m18@yahoo.com>
2022-05-02 00:26:27 +0430

    add UserService

---
+++ src/main/java/maktab74/practice11/org/service/UserService.java
@@ -0,0 +1,7 @@
+package maktab74.practice11.org.service;
+
+import maktab74.practice11.org.base.service.BaseService;
+import maktab74.practice11.org.domain.User;
+
+public interface UserService extends BaseService<User, Long> {
+}
