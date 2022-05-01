8d9cbaf48a84533aff6493e1d37ebc0fe362d1d9
Hossein113 <hos_m18@yahoo.com>
2022-05-02 00:25:57 +0430

    add UserRepository

---
+++ src/main/java/maktab74/practice11/org/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package maktab74.practice11.org.repository;
+
+import maktab74.practice11.org.base.repository.BaseRepository;
+import maktab74.practice11.org.domain.User;
+
+public interface UserRepository extends BaseRepository<User, Long> {
+}
