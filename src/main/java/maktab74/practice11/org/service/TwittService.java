01ded8e58f01c439d5ed19c9c54c82bdd41ae416
Hossein113 <hos_m18@yahoo.com>
2022-05-01 16:54:39 +0430

    update TwittService

--- src/main/java/maktab74/practice11/org/service/TwittService.java
+++ src/main/java/maktab74/practice11/org/service/TwittService.java
@@ -3,5 +3,11 @@ package maktab74.practice11.org.service;
 import maktab74.practice11.org.base.service.BaseService;
 import maktab74.practice11.org.domain.Twitt;
 
+import java.util.List;
+
 public interface TwittService extends BaseService<Twitt, Long> {
+
+    List<Twitt> findByUser(Long id);
+
+    List<Twitt> findAllTwitt();
 }
