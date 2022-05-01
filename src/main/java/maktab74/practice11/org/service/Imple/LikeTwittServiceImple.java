8bccbc01b7bc6be023eeadc61234b89aa9ceacdc
Hossein113 <hos_m18@yahoo.com>
2022-05-01 16:53:04 +0430

    update LikeTwittServiceImple

--- src/main/java/maktab74/practice11/org/service/Imple/LikeTwittServiceImple.java
+++ src/main/java/maktab74/practice11/org/service/Imple/LikeTwittServiceImple.java
@@ -3,10 +3,17 @@ package maktab74.practice11.org.service.Imple;
 import maktab74.practice11.org.base.service.imple.BaseServiceImple;
 import maktab74.practice11.org.domain.LikeTwitt;
 import maktab74.practice11.org.repository.LikeTwittRepository;
+import maktab74.practice11.org.service.LikeTwittService;
 
-public class LikeTwittServiceImple extends BaseServiceImple<LikeTwitt, Long, LikeTwittRepository> {
+public class LikeTwittServiceImple extends BaseServiceImple<LikeTwitt, Long, LikeTwittRepository> implements
+        LikeTwittService {
 
     public LikeTwittServiceImple(LikeTwittRepository repository) {
         super(repository);
+    }
+
+    @Override
+    public void likeOrDislike(LikeTwitt likeTwitt) {
+        repository.likeOrDislike(likeTwitt);
     }
 }
