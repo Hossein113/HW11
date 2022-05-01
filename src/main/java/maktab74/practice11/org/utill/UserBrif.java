a3ec858c01e8c9d44361f0b19fcf7fdf2eb62187
Hossein113 <hos_m18@yahoo.com>
2022-05-01 04:59:41 +0430

    update UserBrif

---
+++ src/main/java/maktab74/practice11/org/utill/UserBrif.java
@@ -0,0 +1,28 @@
+package maktab74.practice11.org.utill;
+
+public class UserBrif {
+
+    private String userName;
+    private String password;
+
+    public UserBrif(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
