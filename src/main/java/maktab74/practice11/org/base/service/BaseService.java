4e8eaa3247cb74a5ee47bd3a8c93aac496aa12dc
Hossein113 <hos_m18@yahoo.com>
2022-07-16 15:54:12 +0330

    add BaseServiceImple

---
+++ src/main/java/maktab74/practice11/org/base/service/BaseService.java
@@ -0,0 +1,31 @@
+package maktab74.practice11.org.base.service;
+
+import maktab74.practice11.org.base.domain.BaseEntity;
+
+import javax.persistence.EntityTransaction;
+import java.io.Serializable;
+import java.util.List;
+
+public interface BaseService<E extends BaseEntity<ID>, ID extends Serializable> {
+
+
+    E saveAndEdit(E e);
+
+    void delelt(ID id);
+
+    Long countAll();
+
+    E findById(ID id);
+
+    List<E> findList();
+
+    EntityTransaction getTransaction();
+
+    void commitTransaction();
+
+    void beginTransaction();
+
+    void rollBackTransaction();
+
+
+}
