05eecb7baa995647f8901af8f62c5ae33ed773e3
Hossein113 <hos_m18@yahoo.com>
2022-07-16 16:01:09 +0330

    add Hibernate

---
+++ src/main/java/maktab74/practice11/org/utill/Hibernate.java
@@ -0,0 +1,17 @@
+package maktab74.practice11.org.utill;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Hibernate {
+
+    private static final EntityManagerFactory emf;
+
+    static {
+        emf = Persistence.createEntityManagerFactory("defult");
+    }
+
+    public EntityManagerFactory getentityManagerFactory() {
+        return emf;
+    }
+}
